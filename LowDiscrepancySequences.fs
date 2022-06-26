2874df476b24f75f395ad73c9f37ac83c94c7d87
B0ryskart0n <boryskopec00@gmail.com>
2022-06-26 13:56:26 +0200

    minor code changes and unification

--- LowDiscrepancySequences.fs
+++ LowDiscrepancySequences.fs
@@ -3,88 +3,88 @@ module LowDiscrepancySequences
 open System
 open System.IO
 open System.Numerics
-// add ndims method
 
-type Additive (dim: int) = 
-    do if dim < 1 then invalidArg "dim" "Sequence dimension must be positive."
+type Additive (nDims: int) = 
+    do if nDims < 1 then invalidArg "nDims" "Sequence dimension must be positive."
 
     let primes = [|2.0; 3.0; 5.0; 7.0; 11.0; 13.0; 17.0; 19.0|]
+    let nDimsMax = Array.length primes
 
-    let dimMax = Array.length primes
-    do if dim > dimMax then invalidArg "dim" <| sprintf "Sequence dimension must be at most %i." dimMax
+    do if nDims > nDimsMax then invalidArg "nDims" <| sprintf "Sequence dimension must be at most %i." nDimsMax
 
     let bases = 
-        match dim with
+        match nDims with
         | 1 -> [|(sqrt 5.0 - 1.0) / 2.0|] // golden ratio
         | _ -> 
             primes
-            |> Array.take dim
+            |> Array.take nDims
             |> Array.map sqrt
-            |> Array.map (fun x -> x % 1.0) // this is not needed but may be faster (to be checked)
+            |> Array.map (fun x -> x % 1.0)
 
-    let u = Array.create dim 0.0
+    let u = Array.create nDims 0.0
 
-    member this.Next () =
-        for i in 0..dim-1 do
-            u[i] <- (u[i] + bases[i]) % 1.0
-        Array.copy u
+    member this.NDims () = nDims
 
-    member this.NextFast () =
-        for i in 0..dim-1 do
-            u[i] <- (u[i] + bases[i]) % 1.0
-        u
-
-// maybe write default (bases: list<int>) and new (dim: int) = Halton (Array.take dim primes)
-type Halton (dim: int) = 
-    do if dim < 1 then invalidArg "dim" "Sequence dimension must be positive."
-
-    let primes = [|2; 3; 5; 7; 11; 13; 17; 19|]
-
-    let dimMax = Array.length primes
-    do if dim > dimMax then invalidArg "dim" <| sprintf "Sequence dimension must be at most %i." dimMax
-
-    let bases = Array.take dim primes
-
-    let n = Array.create dim 0
-    let d = Array.create dim 1
-    let x = Array.create dim 0
-    let y = Array.create dim 0
-
-    let u = Array.create dim 0.0
-
-    member this.Next () = 
-        for i in 0..dim-1 do
-            x[i] <- d[i] - n[i]
-
-            if x[i] = 1 then 
-                n[i] <- 1
-                d[i] <- d[i] * bases[i]
-            else 
-                y[i] <- d[i] / bases[i]
-                while x[i] <= y[i] do 
-                    y[i] <- y[i] / bases[i]
-                n[i] <- (bases[i] + 1) * y[i] - x[i]
-            u[i] <- float n[i] / float d[i]
-        Array.copy u
-
-    member this.NextFast () = 
-        for i in 0..dim-1 do
-            x[i] <- d[i] - n[i]
-
-            if x[i] = 1 then 
-                n[i] <- 1
-                d[i] <- d[i] * bases[i]
-            else 
-                y[i] <- d[i] / bases[i]
-                while x[i] <= y[i] do 
-                    y[i] <- y[i] / bases[i]
-                n[i] <- (bases[i] + 1) * y[i] - x[i]
-            u[i] <- float n[i] / float d[i]
-        u
-    
     member this.Current () = Array.copy u
 
     member this.CurrentFast () = u
+
+    member private this.CalculateNext () = 
+        for i in 0 .. nDims - 1 do 
+            u[i] <- (u[i] + bases[i]) % 1.0
+
+    member this.Next () = 
+        this.CalculateNext ()
+        Array.copy u
+
+    member this.NextFast () = 
+        this.CalculateNext ()
+        u
+
+type Halton (nDims: int) = 
+    do if nDims < 1 then invalidArg "dim" "Sequence dimension must be positive."
+
+    let primes = [|2; 3; 5; 7; 11; 13; 17; 19|]
+    let nDimsMax = Array.length primes
+
+    do if nDims > nDimsMax then invalidArg "nDims" <| sprintf "Sequence dimension must be at most %i." nDimsMax
+
+    let bases = Array.take nDims primes
+
+    let n = Array.create nDims 0
+    let d = Array.create nDims 1
+    let x = Array.create nDims 0
+    let y = Array.create nDims 0
+
+    let u = Array.create nDims 0.0
+
+    member this.NDims () = nDims
+
+    member this.Current () = Array.copy u
+
+    member this.CurrentFast () = u
+
+    member private this.CalculateNext () = 
+        for i in 0 .. nDims - 1 do
+            x[i] <- d[i] - n[i]
+
+            if x[i] = 1 then 
+                n[i] <- 1
+                d[i] <- d[i] * bases[i]
+            else 
+                y[i] <- d[i] / bases[i]
+                while x[i] <= y[i] do 
+                    y[i] <- y[i] / bases[i]
+                n[i] <- (bases[i] + 1) * y[i] - x[i]
+            u[i] <- float n[i] / float d[i]
+
+    member this.Next () = 
+        this.CalculateNext ()
+        Array.copy u
+
+    member this.NextFast () = 
+        this.CalculateNext ()
+        u
 
 module Constats = 
     let aSobol = 
@@ -102,6 +102,7 @@ type Sobol (nDims: int) =
     do if nDims < 1 then invalidArg "nDims" "Sequence dimension must be positive."
 
     let nDimsMax = Array.length aSobol
+
     do if nDims > nDimsMax then invalidArg "nDims" <| sprintf "Sequence dimension must be at most %i." nDimsMax
 
     let m = Array2D.create nDims 32 1u
@@ -113,6 +114,8 @@ type Sobol (nDims: int) =
     let mutable bTemp = 0u
 
     let u = Array.create nDims 0.0
+
+    let bitCastInt32 (a: uint32) = a |> BitConverter.GetBytes |> BitConverter.ToInt32
 
     do if nDims > 1 then
         let mutable a = 0u
@@ -133,9 +136,13 @@ type Sobol (nDims: int) =
                     m[i, j] <- m[i, j] ^^^ (((ac &&& 1u) * m[i, j - d + k]) <<< (d - k))
                     ac <- ac >>> 1
 
-    let bitCastInt32 (a: uint32) = a |> BitConverter.GetBytes |> BitConverter.ToInt32
+    member this.NDims () = nDims
 
-    member this.CalculateNext () = 
+    member this.Current () = Array.copy u
+
+    member this.CurrentFast () = u
+
+    member private this.CalculateNext () = 
         if n = 0u - 1u then failwith "Maximum number of points in the Sobol sequence already reached."
 
         n <- n + 1u
@@ -152,9 +159,9 @@ type Sobol (nDims: int) =
                 u[i] <- float x[i] * 2.0 ** float (bitCastInt32 ~~~cTemp) // should be ldexp (C.math.ldexp)
 
     member this.Next () = 
-        this.CalculateNext () // |> ignore
+        this.CalculateNext ()
         Array.copy u
     
     member this.NextFast () = 
-        this.CalculateNext () // |> ignore
+        this.CalculateNext ()
         u
